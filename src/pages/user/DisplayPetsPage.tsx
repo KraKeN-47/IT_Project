e4d669a2e1deb4ab9f139c90df8e031bf1b6ea8a
Karolis Vaitelė <karvai11@ktu.lt>
2020-12-04 16:24:38 +0200

    Pets page

--- src/pages/user/DisplayPetsPage.tsx
+++ src/pages/user/DisplayPetsPage.tsx
@@ -6,19 +6,50 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TableBody,
+  IconButton,
 } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 import { FormWrapper } from "components";
 import { useHistory } from "react-router";
 import { paths } from "router/paths";
+import { useSelector } from "react-redux";
+import { selectUserId } from "modules/userType/userData.selector";
+import { api } from "global/variables";
 
 export default function DisplayPetsPage() {
   const history = useHistory();
   const handleRedirectAddPet = () => {
     history.push(paths.addPet);
   };
+  const [pets, setPets] = useState<any>([]);
+  const id: any = useSelector(selectUserId());
+  const getPets = () => {
+    api
+      .get(`/Pet/${id}`)
+      .then((resp: any) => {
+        const data: any = resp.data;
+        setPets(data);
+      })
+      .catch((x) => alert(x.response.data));
+  };
+  useEffect(() => {
+    getPets();
+  }, []);
+
+  const removePet = (id: any) => {
+    api
+      .delete(`/Pet/${id}`)
+      .then(() => {
+        alert("Gyvunas istrintas");
+        getPets();
+      })
+      .catch((x) => alert(x.response.data));
+  };
+
   return (
     <FormWrapper>
       <Box
@@ -39,8 +70,29 @@ export default function DisplayPetsPage() {
                 <TableCell align="right">Lytis</TableCell>
                 <TableCell align="right">Amžius</TableCell>
                 <TableCell align="right">Svoris</TableCell>
+                <TableCell></TableCell>
               </TableRow>
             </TableHead>
+            <TableBody>
+              {pets.map((pet) => (
+                <TableRow key={pet.id}>
+                  {console.log(pet)}
+                  <TableCell>{pet.vardas}</TableCell>
+                  <TableCell align="right">{pet.rusis}</TableCell>
+                  <TableCell align="right">{pet.veisle}</TableCell>
+                  <TableCell align="right">
+                    {pet.lytis === 1 ? "Patinas" : "Patelė"}
+                  </TableCell>
+                  <TableCell align="right">{pet.amzius}</TableCell>
+                  <TableCell align="right">{pet.svoris}</TableCell>
+                  <TableCell align="right">
+                    <IconButton onClick={() => removePet(pet.id)}>
+                      <DeleteForeverIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
           </Table>
         </TableContainer>
         <Box mt="20px">
